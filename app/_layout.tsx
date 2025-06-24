bb1e45a91e19377cd0b405f11f9427b141877e63
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-24 13:08:28 +0530

    add ons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
-import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
+import {Ionicons} from "@expo/vector-icons"
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer screenOptions={{
+        drawerContentStyle:{backgroundColor:"black"},
+        drawerActiveTintColor:"white",
+        drawerActiveBackgroundColor:"transparent",
+        drawerHideStatusBarOnOpen:true,
+        drawerLabelStyle:{fontSize:18, fontWeight:'medium', marginLeft:'10%', fontFamily:"Inter"}
+      }}>
+
+        <Drawer.Screen
+          name="index" // This is the name of the page and must match the url from root
+          options={{
+            drawerIcon:({focused})=>(
+              <Ionicons name="home" color="white" size={24} />
+            ),
+            drawerLabel: "Home",
+            title: "overview"
+          }}
+        />
+
+      </Drawer>
+    </GestureHandlerRootView>
+  );
 }
