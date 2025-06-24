fcdae77fe3e5004c72631de13ad14ef2f03020c9
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-24 13:17:01 +0530

    added image as icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,45 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
-import {Ionicons} from "@expo/vector-icons"
+import { Ionicons } from "@expo/vector-icons";
+import {Image}from "expo-image";
+
+const blurhash =
+  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer screenOptions={{
-        drawerContentStyle:{backgroundColor:"black"},
-        drawerActiveTintColor:"white",
-        drawerActiveBackgroundColor:"transparent",
-        drawerHideStatusBarOnOpen:true,
-        drawerLabelStyle:{fontSize:18, fontWeight:'medium', marginLeft:'10%', fontFamily:"Inter"}
-      }}>
-
+      <Drawer
+        screenOptions={{
+          drawerContentStyle: { backgroundColor: "black" },
+          drawerActiveTintColor: "white",
+          drawerActiveBackgroundColor: "transparent",
+          drawerHideStatusBarOnOpen: true,
+          drawerLabelStyle: {
+            fontSize: 18,
+            fontWeight: "medium",
+            marginLeft: "10%",
+          },
+          drawerIcon: ({ focused }) => (
+            <Image
+              source="https://picsum.photos/seed/696/3000/2000"
+              placeholder={{ blurhash }}
+              contentFit="cover"
+              transition={1000}
+            />
+          ),
+        }}
+      >
         <Drawer.Screen
           name="index" // This is the name of the page and must match the url from root
           options={{
-            drawerIcon:({focused})=>(
+            drawerIcon: ({ focused }) => (
               <Ionicons name="home" color="white" size={24} />
             ),
             drawerLabel: "Home",
-            title: "overview"
+            title: "overview",
           }}
         />
-
       </Drawer>
     </GestureHandlerRootView>
   );
