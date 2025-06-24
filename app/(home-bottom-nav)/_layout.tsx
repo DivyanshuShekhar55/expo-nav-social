c38a75e3b77de48a67a3de39f3e3340ff6bd3998
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-24 18:16:16 +0530

    removed ripple

--- app/(home-bottom-nav)/_layout.tsx
+++ app/(home-bottom-nav)/_layout.tsx
@@ -1,26 +1,32 @@
 import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
+import { Pressable, TouchableWithoutFeedback, View } from 'react-native';
 
 export default function TabLayout() {
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'black',
       tabBarInactiveTintColor:"black",
       headerShown:false,
-      
+     tabBarButton: (props) => (
+      <Pressable {...props} android_ripple={null}>
+        <View style={{ flex: 1, alignItems:"center" }}>{props.children}</View>
+      </Pressable>
+    ),
      }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="home-outline" color={color} />,
+          tabBarIcon: ({ color, focused }) => <Ionicons size={28} name={!focused?"home-outline":"home"} color={"black"} />,
+          
         }}
       />
       <Tabs.Screen
         name="Notifications"
         options={{
           title: 'Notifications',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="notifications-outline" color={color} />,
+          tabBarIcon: ({ color, focused }) => <Ionicons size={28} name={!focused?"notifications-outline":"notifications"} color={"black"} />,
         }}
       />
     </Tabs>
