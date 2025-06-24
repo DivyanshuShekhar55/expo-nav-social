39abee8ff1290232d7f4cf1675e017295a9c84e2
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-24 17:37:53 +0530

    added text and ui changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,50 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
 import { Ionicons } from "@expo/vector-icons";
-import {Image}from "expo-image";
+import { Image } from "expo-image";
+import { TouchableOpacity, View, Text } from "react-native";
 
 const blurhash =
-  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer
-        screenOptions={{
+        screenOptions={({ navigation }) => ({
           drawerContentStyle: { backgroundColor: "black" },
           drawerActiveTintColor: "white",
           drawerActiveBackgroundColor: "transparent",
           drawerHideStatusBarOnOpen: true,
+          headerTitle: () => (
+            <Text style={{ color: "white", fontSize: 18 }}>MySocial</Text>
+          ),
+          headerBackground: () => (
+            <View style={{ flex: 1, backgroundColor: "black" }} />
+          ),
           drawerLabelStyle: {
             fontSize: 18,
             fontWeight: "medium",
             marginLeft: "10%",
           },
-          drawerIcon: ({ focused }) => (
-            <Image
-              source="https://picsum.photos/seed/696/3000/2000"
-              placeholder={{ blurhash }}
-              contentFit="cover"
-              transition={1000}
-            />
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.openDrawer();
+              }}
+              style={{ paddingHorizontal: 18 }}
+            >
+              <Image
+                source="https://picsum.photos/seed/696/3000/2000"
+                placeholder={{ blurhash }}
+                contentFit="cover"
+                transition={1000}
+                aria-hidden
+                style={{ width: 30, height: 30, borderRadius: 30 }}
+              />
+            </TouchableOpacity>
           ),
-        }}
+        })}
       >
         <Drawer.Screen
           name="index" // This is the name of the page and must match the url from root
